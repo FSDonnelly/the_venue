cdaed95ad5946773a9b140ed6beec54143b2705c
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-22 16:29:16 -0700

    added images to carrousel

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
+import Featured from './components/featured';
 
 function App() {
   return (
-    <div className='App'>
+    <div
+      className='App'
+      style={{ height: '15000px', backgroundColor: 'lightgreen' }}
+    >
       <Header />
+      <Featured />
     </div>
   );
 }
--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -9,7 +9,24 @@ import SideDrawer from './SideDrawer';
 
 class Header extends Component {
   state = {
-    drawerOpen: false
+    drawerOpen: false,
+    headerShow: false
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (window.scrollY > 0) {
+      this.setState({
+        headerShow: true
+      });
+    } else {
+      this.setState({
+        headerShow: false
+      });
+    }
   };
 
   toggleDrawer = value => {
@@ -23,9 +40,10 @@ class Header extends Component {
       <AppBar
         position='fixed'
         style={{
-          backgroundColor: '#2f2f2f',
+          backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
           boxShadow: 'none',
-          padding: '10px 0ox'
+          padding: '10px 0ox',
+          transition: 'all 300ms ease-in'
         }}
       >
         <ToolBar>
