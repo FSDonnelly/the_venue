4cb4189e168c45ad028abc79eb29de409e10ac18
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-21 22:59:13 -0700

    started header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './resources/styles.css';
+
+import Header from './components/header_footer/Header';
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
